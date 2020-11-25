822c24942f5f7903518d05e057a466589930d377
Przemysław Chodór <prz.chodor@gmail.com>
2020-11-25 13:29:57 +0100

    nowa gałąź

--- app/src/androidTest/java/com/example/canvastest/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/canvastest/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.inzynieria_oprogramowania
+package com.example.canvastest
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.inzynieria_oprogramowania", appContext.packageName)
+        assertEquals("com.example.canvastest", appContext.packageName)
     }
 }
---
+++ app/src/main/java/com/example/canvastest/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.canvastest
+
+import android.gesture.Gesture
+import android.opengl.GLES20
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.widget.Toast
+import androidx.core.view.GestureDetectorCompat
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
